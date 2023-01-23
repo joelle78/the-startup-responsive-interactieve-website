455a220f014590a805349b0e6b6f4901a9b0fd10
joelle78 <96306220+joelle78@users.noreply.github.com>
2023-01-24 00:06:27 +0100

    progress

--- styles/script.js
+++ styles/script.js
@@ -14,7 +14,7 @@ function carousel() {
     setTimeout(carousel, 3000); // Change image every 2 seconds
 }
 
-// Reveal in function intro text
+// Reveal in function intro text (onder naar boven)
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
 
@@ -32,3 +32,42 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+// Reveal in function intro text 2 (rechts naar links)
+function reveal2() {
+    var reveals = document.querySelectorAll(".reveal2");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal2);
+
+// Reveal in function intro text 2 (rechts naar links)
+function reveal3() {
+    var reveals = document.querySelectorAll(".reveal3");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal3);
+
